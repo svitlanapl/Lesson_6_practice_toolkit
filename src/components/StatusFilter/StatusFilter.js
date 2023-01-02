8c9e3a0a8e62cd646fa67438503de956791f52c0
svitlanapl <sveta_pr@i.ua>
2023-01-02 20:44:01 +0200

    add planner Redux

---
+++ src/components/StatusFilter/StatusFilter.js
@@ -0,0 +1,48 @@
+// Імпортуємо хук
+import { useSelector, useDispatch } from "react-redux";
+
+// Імпортуємо генератор екшену
+import { setStatusFilter } from "redux/actions";
+
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from "redux/constants";
+
+import { getStatusFilter } from "redux/selectors";
+
+import { Button } from "components/Button/Button";
+import css from "./StatusFilter.module.css";
+
+export const StatusFilter = () => {
+    // Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(getStatusFilter);
+
+  // Викликаємо генератор екшену та передаємо значення фільтра
+  // Відправляємо результат - екшен зміни фільтра
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
+
+  return (
+    <div className={css.wrapper}>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
+    </div>
+  );
+};
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore } from "redux";
+import { devToolsEnhancer } from "@redux-devtools/extension";
+import { rootReducer } from "./reducer";
+
+// Створюємо розширення стора, щоб додати інструменти розробника
+const enhancer = devToolsEnhancer();
+
+export const store = createStore(rootReducer, enhancer);
